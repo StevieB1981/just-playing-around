1bbc88376ac46932d2d301735c95f088f6e84972
StevieB1981 <steve.brownlow@coveros.com>
2024-11-01 14:34:00 -0400

    My first commit

---
+++ first_one.py
@@ -0,0 +1,14 @@
+def steve_function(): 
+    print("Howdy from my Function!")
+
+def my_function_with_args(username, greeting) 
+    print("Hey, %s, from Steve's Function!, Be careful %s (username, gretting)) 
+
+def product_three_numbers(a, b, c):
+    return a * b * C
+
+steve_function()
+
+steve_function_ with_args("Bryce Brownlow", "with what you doing!")
+
+x = product_three_numbers( 4, 12, 20) 
